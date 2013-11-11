cf3ddf26545e8ffc4798577b2514a4589b0782c4
Alistair Miles <alimanfoo@googlemail.com>
2013-11-11 12:44:10 +0000

    v0.2

---
+++ release.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+version=`grep __version__ vcfplt.py | sed -e "s/__version__[ ]=[ ]'\(.*\)'/\1/"`
+echo $version
+# git commit and push
+git add --all
+git commit -m v$version
+git push
+# git tag and push
+git tag -a v$version -m v$version
+git push --tags
+# update pypi
+python setup.py register sdist upload
--- vcfplt.py
+++ vcfplt.py
@@ -1,5 +1,5 @@
 __author__ = 'alimanfoo@googlemail.com'
-__version__ = '0.1'
+__version__ = '0.2'
 
 
 import numpy as np
